d0b6b9fc1b1d8ed82748246986cc6ed64a72fb29
AnatoliyDanets <danetsxp@gmail.com>
2021-12-23 22:52:57 +0200

    add hw3

---
+++ model/contact.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+const Joi = require('joi');
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Set name for contact'],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Contact = model('contact', contactSchema);
+
+const schema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'net', 'uk', 'org', 'ua', 'ru'] },
+    })
+    .required(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean(),
+});
+module.exports = { Contact, schema };
